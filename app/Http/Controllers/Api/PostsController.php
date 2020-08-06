dea8a98b565b42c61123b9d7db934b6d0a9e2d47
Autorooter <hadi.assalem@gmail.com>
2020-08-06 18:19:59 +0200

    Add Posts Seeder, Factory

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -4,30 +4,14 @@ namespace App\Http\Controllers\Api;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Post;
 
 class PostsController extends Controller
 {
 
     public function index()
     {
-        $posts = [
-            [
-                "Title" => "Title 1",
-                "Body" => "Body 1"
-            ],
-            [
-                "Title" => "Title 2",
-                "Body" => "Body 2"
-            ],
-            [
-                "Title" => "Title 3",
-                "Body" => "Body 3"
-            ],
-            [
-                "Title" => "Title 4",
-                "Body" => "Body 4"
-            ]
-        ];
+        $posts = Post::get();
 
         return response($posts, 200);
     }
