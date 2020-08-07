a201e2ec8c07304bb1f93bdf6b79520623cd01f5
Autorooter <hadi.assalem@gmail.com>
2020-08-07 16:56:35 +0200

    Add Post Validator

--- app/Http/Controllers/Api/PostsController.php
+++ app/Http/Controllers/Api/PostsController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Post;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 
 class PostsController extends Controller
@@ -36,10 +37,37 @@ class PostsController extends Controller
 
 
     public function store(Request $request){
+
+        // Validation (1): 
+        //        - Pros: Faster, and return Specific Error MSG
+        //        - Cons: What you will do if the Form contains 100 Fields ?
+
+        // if(!$request->has('title') && $request->get('title') == ''){
+        //     return $this->apiResponse(null, "Title Is Required", 422);
+        // }
+
+        // if(!$request->has('body') && $request->get('body') == ''){
+        //     return $this->apiResponse(null, "Body Is Required", 422);
+        // }
+
+
+        // Validation (2): Laravel Validation
+
+        $validate = Validator::make($request->all(), [
+            'title' => 'required|min:5|max:199',
+            'body' => 'required|min:15|max:1500'
+        ]);
+
+        if($validate->fails()){
+            return $this->apiResponse(null, $validate->errors(), 422);
+        }
+
         $post = Post::create($request->all());
+
         if($post){
             return $this->apiResponse(new PostResource($post), null, 201);
         }
-        return $this->apiResponse(null, "Un-known Error", 400);
+
+        return $this->apiResponse(null, "Un-known Error", 520);
     }
 }
